122e7d5f8ad6291868f37988141d9f001d83f626
buddhika4u <buddhika4u98@gmail.com>
2023-04-23 23:37:52 +0530

    Create BMI Calculator

--- lib/Screens/home/home.dart
+++ lib/Screens/home/home.dart
@@ -19,23 +19,62 @@ class _HomeState extends State<Home> {
     'assests/RuhunaHome1.jpg',
     'assests/RuhunaHome2.jpg',
     'assests/RuhunaHome3.jpg',
-
   ];
+
+  double _height = 150.0;
+  double _weight = 54.0;
+  double _bmi = 0.0;
+
+  void _calculateBMI() {
+    setState(() {
+      _bmi = _weight / ((_height / 100.0) * (_height / 100.0));
+    });
+  }
+
+  String getBMICategory() {
+    if (_bmi < 18.5) {
+      return 'Underweight';
+    } else if (_bmi < 25) {
+      return 'Normal';
+    } else if (_bmi < 30) {
+      return 'Overweight';
+    } else if (_bmi < 35) {
+      return 'Obese';
+    } else {
+      return 'Extremely Obese';
+    }
+  }
+
+  Color getBMIColor() {
+    if (_bmi < 18.5) {
+      return Colors.blue;
+    } else if (_bmi < 25) {
+      return Colors.green;
+    } else if (_bmi < 30) {
+      return Colors.yellow;
+    } else if (_bmi < 35) {
+      return Colors.orange;
+    } else {
+      return Colors.red;
+    }
+  }
 
   @override
   void initState() {
     super.initState();
-   _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
       setState(() {
         _currentIndex = (_currentIndex + 1) % _imageUrls.length;
       });
     });
   }
+
   @override
   void dispose() {
     _timer.cancel();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -66,7 +105,7 @@ class _HomeState extends State<Home> {
           children: [
             // Can Add SizedBox or Container
             Container(
-              width:double.infinity,
+              width: double.infinity,
               height: 250,
               decoration: BoxDecoration(
                 image: DecorationImage(
@@ -76,7 +115,7 @@ class _HomeState extends State<Home> {
                 borderRadius: BorderRadius.circular(10),
               ),
               child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 1,sigmaY: 1),
+                filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
                 child: Container(
                   decoration: BoxDecoration(
                     color: Colors.blue.withOpacity(0.3),
@@ -101,9 +140,9 @@ class _HomeState extends State<Home> {
                       const SizedBox(height: 20),
                       const Text(
                         'All Registered students and staff are entitled to '
-                            'free consultations, free basic medicinal drugs, '
-                            'free laboratory services and other required '
-                            'health services.',
+                        'free consultations, free basic medicinal drugs, '
+                        'free laboratory services and other required '
+                        'health services.',
                         textAlign: TextAlign.center,
                         style: TextStyle(color: Colors.black, fontSize: 15),
                       ),
@@ -284,6 +323,105 @@ class _HomeState extends State<Home> {
               ),
             ),
             const SizedBox(height: 10),
+
+            Container(
+              padding: const EdgeInsets.all(20),
+              margin: const EdgeInsets.fromLTRB(10, 20, 10, 20),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'Calculate your BMI',
+                    style: TextStyle(
+                        fontSize: 22,
+                        color: Color(0xFF0D47A1),
+                        fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Height (cm): ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            setState(() {
+                              _height = double.tryParse(value) ?? _height;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Weight (kg): ',
+                        style: TextStyle(fontSize: 20),
+                      ),
+                      SizedBox(
+                        width: 100,
+                        child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            setState(() {
+                              _weight = double.tryParse(value) ?? _weight;
+                            });
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  SizedBox(
+                    width: 150,
+                    child: ElevatedButton(
+                      onPressed: _calculateBMI,
+                      style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(Colors.grey),
+                      ),
+                      child: const Text('Calculate',
+                          style: TextStyle(fontSize: 18)),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'BMI: ${_bmi.toStringAsFixed(2)}',
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        '(${getBMICategory()})',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: getBMIColor(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                ],
+              ),
+            ),
 
             // Container(
             //   height: 200,
