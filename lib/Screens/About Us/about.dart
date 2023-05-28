c12cc746fca4eef41bac3dc86cf06304f009aea3
buddhika4u <buddhika4u98@gmail.com>
2023-05-29 01:30:10 +0530

    Implement About page

--- lib/Screens/About Us/about.dart
+++ lib/Screens/About Us/about.dart
@@ -1,8 +1,5 @@
-
 import 'package:flutter/material.dart';
 import '../drawer/sidemenu.dart';
-
-
 
 class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
@@ -12,25 +9,20 @@ class About extends StatefulWidget {
 }
 
 class _HomeState extends State<About> {
-
-
   @override
   void initState() {
     super.initState();
-
-
   }
+
   @override
   Widget build(BuildContext context) {
-
-
     return SafeArea(
       child: Scaffold(
         drawer: const SideMenu(),
         appBar: PreferredSize(
           preferredSize: const Size.fromHeight(100.0),
           child: AppBar(
-            title: const Text("About Us"),
+            title: const Text("About"),
             backgroundColor: const Color(0xff011422),
             toolbarHeight: 100,
           ),
@@ -42,7 +34,6 @@ class _HomeState extends State<About> {
               Container(
                 height: 200,
                 width: double.infinity,
-                //width: ,
                 decoration: BoxDecoration(
                   gradient: LinearGradient(
                     begin: Alignment.topCenter,
@@ -52,45 +43,85 @@ class _HomeState extends State<About> {
                       Colors.blueGrey.shade300,
                     ],
                   ),
-                  //color: Colors.green // This code is used to insert sngle background color behind profile
                 ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children:   [
+                  children: [
                     SizedBox(
                       height: 90,
                       width: 90,
-
-                      child: Image.asset('assests/newruh1.png',),
+                      child: Image.asset('assests/newruh1.png'),
                     ),
                     const SizedBox(height: 10),
                     const Text(
                       "UNIVERSITY OF RUHUNA",
                       style: TextStyle(
-                        fontSize: 22,
+                        fontSize: 24,
                         fontWeight: FontWeight.bold,
                         color: Colors.black,
                       ),
                     ),
                     const SizedBox(height: 5),
                     const Text(
-                      "DEPARTMENT OF COMPUTER SCIENCE",
+                      "MEDICAL CENTER",
                       style: TextStyle(
-                        fontSize: 16,
+                        fontSize: 18,
                         color: Colors.black87,
                       ),
                     ),
                   ],
                 ),
               ),
-              const SizedBox(height: 100,),
-
+              const SizedBox(height: 15),
+              Expanded(
+                child: ListView(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  children: const [
+                    Text(
+                      "About Us",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF0D47A1),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      "The University Comprehensive Medical Center is a leading healthcare provider, offering a wide range of medical services, including primary care, specialized treatments, diagnostic testing, and advanced surgical procedures. Our state-of-the-art facilities and experienced medical professionals ensure that you receive the best possible care. We are committed to providing personalized and compassionate care to all our patients, with a focus on promoting overall health and well-being.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blueAccent,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: 15),
+                    Text(
+                      "Our History",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Color(0xFF0D47A1),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 12),
+                    Text(
+                      "The University Comprehensive Medical Center has been a leader in healthcare for decades, with a rich history of innovation and excellence. We are committed to providing compassionate and personalized care to all our patients, with a focus on advancing medical research and education. Our team of expert physicians and researchers are dedicated to improving health outcomes and enhancing the well-being of our community. Trust us for world-class healthcare services and a patient-centric approach that puts your needs first.",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.blueAccent,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
             ],
           ),
         ),
       ),
     );
   }
-
 }
-
